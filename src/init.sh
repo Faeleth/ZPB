2b1aa3b3c8f35a4e3031c02450407133fa24f73d
Calmeon <jakubgebicz51@gmail.com>
2025-10-19 13:50:14 +0200

    Initial project setup

---
+++ src/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create virtual enviroment:
+# python -m venv venv
+# Source in your terminal to run proj enviroment:
+# source venv/Scripts/activate
+
+# Then run this sricpt to install python packages using:
+# ./init.sh
+# This tells pip to install everything listed in the file
+pip install -r requirements.txt
+
+# Run project using:
+# pyhon main.py
--- src/main.py
+++ src/main.py
@@ -2,6 +2,13 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+import database_loader as dl
+
+DATASET_ROOT_DIR = ".\..\YOLO_format"
+VERBOSE = True
+
 
 if __name__ == "__main__":
-    print("Hello, World!")
+    (data_info, train_data, valid_data, test_data) = dl.load_yolo_dataset(
+        DATASET_ROOT_DIR, VERBOSE
+    )
