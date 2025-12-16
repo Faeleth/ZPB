f263f5fa88b01b4cd3884ef2f23ce8034c9a4eb9
Na-ta-nek <bienkowskikrzysztof@wp.pl>
2025-12-16 17:34:46 +0100

    Plot management, cleaning before closing

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import numpy as np
 import customtkinter as ctk
 import cv2
 from PIL import Image
 from FaceRecognition import FaceRecognition
+from Plot import Plot
 from tkinter import filedialog
 
 
@@ -10,10 +14,15 @@ class App(ctk.CTk):
         super().__init__()
 
         self.init_window()
+        self.protocol("WM_DELETE_WINDOW", self.on_close)
+        self.after_ids = []
 
         self.model = FaceRecognition(
             "./results/yolo11x_training_epochs300_128/weights/best.pt"
         )
+
+        self.plot = Plot(100)
+        self.text_objects = {}
 
         self.cap = None
         self.is_running = False
@@ -91,11 +100,11 @@ class App(ctk.CTk):
         )
         self.plot_placeholder.pack(fill="x", padx=10, pady=10)
 
-        self.label_stat_1 = ctk.CTkLabel(self.sidebar_frame, text="Emotion: -")
-        self.label_stat_1.pack(pady=5)
+        # self.label_stat_1 = ctk.CTkLabel(self.sidebar_frame, text="Emotion: -")
+        # self.label_stat_1.pack(pady=5)
 
-        self.label_stat_2 = ctk.CTkLabel(self.sidebar_frame, text="Confidence: -")
-        self.label_stat_2.pack(pady=5)
+        # self.label_stat_2 = ctk.CTkLabel(self.sidebar_frame, text="Confidence: -")
+        # self.label_stat_2.pack(pady=5)
 
         self.btn_start_stop = ctk.CTkButton(
             self.sidebar_frame,
@@ -104,6 +113,56 @@ class App(ctk.CTk):
             height=40,
         )
         self.btn_start_stop.pack(side="bottom", fill="x", padx=10, pady=20)
+
+        self.create_bar_chart()
+        
+        self.bind("<Configure>", self.resize_plot)
+
+    # ============================================================
+    # PLOT
+    # ============================================================
+    def create_bar_chart(self):
+        fig, ax = plt.subplots(figsize=(6, 3))
+        fig.subplots_adjust(left=0.24)
+
+        self.emotions = ["Anger",
+                    "Contempt",
+                    "Disgust",
+                    "Fear",
+                    "Happy",
+                    "Neutral",
+                    "Sad",
+                    "Surprise",]
+        values = np.zeros(len(self.emotions))
+        
+        self.bars = ax.barh(self.emotions, values, color="#2196F3")
+        
+        ax.set_xlim(0, 20)
+        
+        ax.set_xlabel("Quantity")
+
+        fig.patch.set_facecolor('#2b2b2b')
+        ax.spines['top'].set_color('#2b2b2b')
+        ax.spines['right'].set_color('#2b2b2b')
+        ax.spines['left'].set_color('#2196F3')
+        ax.spines['bottom'].set_color('#2196F3')
+        ax.set_facecolor("#2b2b2b")
+        ax.grid(False)
+
+        self.canvas = FigureCanvasTkAgg(fig, master=self.plot_placeholder)
+        self.canvas.get_tk_widget().pack(fill="both", expand=True)
+        self.canvas.draw()
+
+        self.fig = fig
+        self.ax = ax
+    
+    def resize_plot(self, event):
+        window_height = self.sidebar_frame.winfo_height()
+        plot_height = window_height - 156
+        self.plot_placeholder.configure(height=plot_height)
+        left_value = 0.15 + ((1 / self.top_frame.winfo_width()) * 100)
+        left_value = max(0, min(left_value, 0.85))
+        self.fig.subplots_adjust(left=left_value)
 
     def load_camera(self):
         if not self.is_running:
@@ -131,15 +190,20 @@ class App(ctk.CTk):
             self.btn_start_stop.configure(text="Stop")
 
     def camera_loop(self):
-        if self.is_running and self.cap:
-            ret, frame = self.cap.read()
-            if ret:
-                self.display_image(frame)
+        if not self.is_running or not self.cap or not self.winfo_exists():
+            return
 
-            self.after(10, self.update_frame)  # 10 ms
+        ret, frame = self.cap.read()
+        if ret:
+            self.display_image(frame)
+
+        after_id = self.after(10, self.update_frame)
+        self.after_ids.append(after_id)
 
     def display_image(self, frame):
-        frame_with_recognition = self.model.predict(frame)
+        frame_with_recognition, plot_data = self.model.predict(frame)
+        class_counts = self.plot.update(plot_data)
+        self.update_plot(class_counts)
         pil_image = Image.fromarray(frame_with_recognition)
         ctk_img = ctk.CTkImage(
             light_image=pil_image, dark_image=pil_image, size=(640, 480)
@@ -148,7 +212,51 @@ class App(ctk.CTk):
         self.video_label.image = ctk_img  # Garbage Collector protection
 
     def update_frame(self):
+        if not self.is_running or not self.winfo_exists():
+            return
+    
         self.camera_loop()
+
+    def update_plot(self, class_counts):
+        new_values = [item[0] for item in class_counts.values()]
+        new_confidences = [item[1] for item in class_counts.values()]
+
+        for bar, emotion, count, confidence in zip(self.bars, self.emotions, new_values, new_confidences):
+            if emotion in self.text_objects:
+                if count < 10:
+                    self.text_objects[emotion].remove()
+                    del self.text_objects[emotion]
+            if count > 10:
+                if emotion in self.text_objects:
+                    self.text_objects[emotion].remove()
+                self.text_objects[emotion] = self.ax.text(
+                    bar.get_width() / 2,
+                    bar.get_y() + bar.get_height() / 2,
+                    f'{confidence:.4f}',
+                    va='center',
+                    ha='center',
+                    color='black',
+                    fontsize=10
+                )
+            bar.set_width(count)
+
+        self.ax.set_xlim(0, max(20, max(new_values) + 10))
+        self.canvas.draw()
+
+    def on_close(self):
+        self.is_running = False
+
+        for aid in self.after_ids:
+            try:
+                self.after_cancel(aid)
+            except Exception:
+                pass
+        self.after_ids.clear()
+
+        if self.cap:
+            self.cap.release()
+
+        self.quit()
 
 
 if __name__ == "__main__":
